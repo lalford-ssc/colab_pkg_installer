317b67d6cd33fb8b3677bcbfdabeb55627d63ee9
Latoya Alford <latoya.alford@securityscorecard.io>
2024-07-26 11:19:37 -0400

    added installer pkg files

---
+++ colab_pkg_installer/__init__.py
@@ -0,0 +1,6 @@
+"""
+Colab Package Installer (colab_pkg_installer)
+====================
+colab_pkg_installer is a package that can aid in properly installing custom python packages hosted in github
+into a colab worksheet.
+"""
